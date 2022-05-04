38a2097c2dc566747516d514d60484cea4b8b50d
ShanJuy <tsigel@vikivostok.ru>
2022-05-04 08:57:38 +0300

    style.display from 'none' to 'table-row' to feet total row, in 'none' - feet one column two datepickers dom't work in the same page, assume that because of id= datepicker4

--- razvozki/static/razvozki/rzv.js
+++ razvozki/static/razvozki/rzv.js
@@ -9,6 +9,22 @@ function upd_ln_reverse(cst_){
     cst2 = 'cst_id2' + cst_;
     document.getElementById(cst).style.display='block';
     document.getElementById(cst2).style.display='none';}
+D
+function upd_ln2(cst_){
+    cst = 'cst_id' + cst_;
+    cst2 = 'cst_id2' + cst_;
+    cst3 = 'cst_id3' + cst_;
+    document.getElementById(cst).style.display='none';
+    document.getElementById(cst2).style.display='table-row';
+    document.getElementById(cst3).style.display='table-row';}
+
+function upd_ln2_reverse(cst_){
+    cst = 'cst_id' + cst_;
+    cst2 = 'cst_id2' + cst_;
+    cst3 = 'cst_id3' + cst_;
+    document.getElementById(cst).style.display='block';
+    document.getElementById(cst2).style.display='none';
+    document.getElementById(cst3).style.display='none';}
 
 function add_ln(new_ln){
     document.getElementById(new_ln).style.display='block';}
