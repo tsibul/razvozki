87ec54b7e0022d1e414f11e57eb4f4c3fe54714a
ShanJuy <tsigel@vikivostok.ru>
2022-05-04 14:52:40 +0300

    Error in js upd_ln because cst_id(16) and cst_id1(6) the same value, changed to cst_id_

--- razvozki/static/razvozki/rzv.js
+++ razvozki/static/razvozki/rzv.js
@@ -1,27 +1,27 @@
 function upd_ln(cst_){
-    cst = 'cst_id' + cst_;
-    cst2 = 'cst_id2' + cst_;
+    cst = 'cst_id_' + cst_;
+    cst2 = 'cst_id2_' + cst_;
     document.getElementById(cst).style.display='none';
     document.getElementById(cst2).style.display='block';}
 
 function upd_ln_reverse(cst_){
-    cst = 'cst_id' + cst_;
-    cst2 = 'cst_id2' + cst_;
+    cst = 'cst_id_' + cst_;
+    cst2 = 'cst_id2_' + cst_;
     document.getElementById(cst).style.display='block';
     document.getElementById(cst2).style.display='none';}
 D
 function upd_ln2(cst_){
-    cst = 'cst_id' + cst_;
-    cst2 = 'cst_id2' + cst_;
-    cst3 = 'cst_id3' + cst_;
+    cst = 'cst_id_' + cst_;
+    cst2 = 'cst_id2_' + cst_;
+    cst3 = 'cst_id3_' + cst_;
     document.getElementById(cst).style.display='none';
     document.getElementById(cst2).style.display='table-row';
     document.getElementById(cst3).style.display='table-row';}
 
 function upd_ln2_reverse(cst_){
-    cst = 'cst_id' + cst_;
-    cst2 = 'cst_id2' + cst_;
-    cst3 = 'cst_id3' + cst_;
+    cst = 'cst_id_' + cst_;
+    cst2 = 'cst_id2_' + cst_;
+    cst3 = 'cst_id3_' + cst_;
     document.getElementById(cst).style.display='block';
     document.getElementById(cst2).style.display='none';
     document.getElementById(cst3).style.display='none';}
