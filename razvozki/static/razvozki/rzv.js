d4fcfd619656c6eb0e8dcd82de603bc77f60f095
ShanJuy <tsigel@vikivostok.ru>
2022-05-04 23:30:53 +0300

    Changed add_razv to js

--- razvozki/static/razvozki/rzv.js
+++ razvozki/static/razvozki/rzv.js
@@ -9,7 +9,7 @@ function upd_ln_reverse(cst_){
     cst2 = 'cst_id2_' + cst_;
     document.getElementById(cst).style.display='block';
     document.getElementById(cst2).style.display='none';}
-D
+
 function upd_ln2(cst_){
     cst = 'cst_id_' + cst_;
     cst2 = 'cst_id2_' + cst_;
@@ -22,7 +22,7 @@ function upd_ln2_reverse(cst_){
     cst = 'cst_id_' + cst_;
     cst2 = 'cst_id2_' + cst_;
     cst3 = 'cst_id3_' + cst_;
-    document.getElementById(cst).style.display='block';
+    document.getElementById(cst).style.display='table-row';
     document.getElementById(cst2).style.display='none';
     document.getElementById(cst3).style.display='none';}
 
@@ -31,3 +31,15 @@ function add_ln(new_ln){
 
 function add_ln_reverse(new_ln){
     document.getElementById(new_ln).style.display='none';}
+
+function add_ln2(cst_){
+    cst4 = 'cst_id4_' + cst_;
+    cst5 = 'cst_id5_' + cst_;
+    document.getElementById(cst4).style.display='table-row';
+    document.getElementById(cst5).style.display='table-row';}
+
+function add_ln2_reverse(cst_){
+    cst4 = 'cst_id4_' + cst_;
+    cst5 = 'cst_id5_' + cst_;
+    document.getElementById(cst4).style.display='none';
+    document.getElementById(cst5).style.display='none';}
