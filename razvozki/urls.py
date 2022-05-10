80f99992928762d32278f803d63112cd58c8c327
ShanJuy <tsigel@vikivostok.ru>
2022-05-10 08:56:02 +0300

    customer_clr for different repairs changed pass for updaterecord_cst

--- razvozki/urls.py
+++ razvozki/urls.py
@@ -1,25 +1,24 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 app_name = 'razvozki'
+
 urlpatterns = [
     path('', views.index, name='index'),
-#    path('specifics/<int:razvozki_id>/', views.detail, name='detail'),
-#   path('add_razv/<int:id>/', views.add_razv, name='add_razv'),
     path('addrecord_razv/', views.addrecord_razv, name='addrecord_razv'),
     path('delete_rzv/<int:id>', views.delete_rzv, name='delete_rzv'),
-#    path('update_rzv/<int:id>', views.update_rzv, name='update_rzv'),
     path('updaterecord_rzv/<int:id>', views.updaterecord_rzv, name='updaterecord_rzv'),
-#    path('<str:razvozka_date>/', views.date_detail, name='date_detail'),
-#    path('<int:razvozki>/results/', views.results, name='results'),
     path('main', views.main_rzv, name='main_rzv'),
     path('newdate_rzv', views.newdate_rzv, name='newdate_rzv'),
     path('customers', views.customers, name='customers'),
-#    path('add_cst', views.add_cst, name='add_cst'),
+    path('customers_clr', views.customers_clr, name='customers_clr'),
     path('addrecord_cst/', views.addrecord_cst, name='addrecord_cst'),
     path('delete_cst/<int:id>', views.delete_cst, name='delete_cst'),
-    path('updaterecord_cst/<int:id>', views.updaterecord_cst, name='updaterecord_cst'),
+    path('updaterecord_cst/<from_where>', views.updaterecord_cst, name='updaterecord_cst'),
     path('print/<str:date_r>', views.print, name='print'),
-
+    path('double/', views.double, name='double'),
+    path('double/unite_cst/', views.unite_cst, name='unite_cst'),
 ]
+
+
