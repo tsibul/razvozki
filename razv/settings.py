3c51df25ce7765c58778ededd1e86b123cdfae04
ShanJuy <tsigel@vikivostok.ru>
2022-05-22 12:48:46 +0300

    Allowed host

--- razv/settings.py
+++ razv/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = sec_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.0.1.16']
 
 
 # Application definition
