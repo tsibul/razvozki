35d827b7606919b9b84c3c37cd4338eb2673098c
ShanJuy <tsigel@vikivostok.ru>
2022-05-22 12:34:23 +0300

    Eliminate polls

--- razv/settings.py
+++ razv/settings.py
@@ -39,7 +39,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'razvozki.apps.RazvozkiConfig',
-    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
