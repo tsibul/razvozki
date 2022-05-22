976318e32c5d9b0b6a2f76d8fb03d54e8eb43d6c
ShanJuy <tsigel@vikivostok.ru>
2022-05-22 12:38:57 +0300

    Eliminate polls

--- razv/urls.py
+++ razv/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
     path('razvozki/', include('razvozki.urls')),
     path('admin/', admin.site.urls),
 ]
