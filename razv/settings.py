b5c32c5e127db4fb58c12146b1cbf35e63ee4574
ShanJuy <tsigel@vikivostok.ru>
2022-06-24 22:09:09 +0300

    just test

--- razv/settings.py
+++ razv/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = sec_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['10.0.1.16', '127.0.0.1']
 
