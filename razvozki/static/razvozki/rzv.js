7ead42b4e57560e5a4b426857492573098c8cd8b
ShanJuy <tsigel@vikivostok.ru>
2022-08-09 21:28:51 +0300

    add collapse

--- razvozki/static/razvozki/rzv.js
+++ razvozki/static/razvozki/rzv.js
@@ -396,3 +396,17 @@ function cst_list(date_r) {
 
     document.getElementById(line_id).innerHTML = code_html;
 }
+
+
+function colapse(id){
+    var area = 'collapse_' + id;
+    var button = area + '_but';
+    var button_minus = '<button type=button class="btn btn-sm btn-outline-secondary" ><i class="fas fa-minus"></i></button>';
+    var button_plus = '<button type=button class="btn btn-sm btn-outline-success" ><i class="fas fa-plus"></i></button>';
+    if (document.getElementById(area).style.display =='none'){
+    document.getElementById(button).innerHTML = button_minus;
+    document.getElementById(area).style.display = 'table-row-group';}
+    else {document.getElementById(button).innerHTML = button_plus;
+    document.getElementById(area).style.display = 'none';};
+
+}
