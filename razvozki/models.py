2b821c040cf046f60c6a5e1fbce190a449f685e4
ShanJuy <tsigel@vikivostok.ru>
2022-08-23 10:27:51 +0300

    initial commit ver 2.0
    new fields for plan date_until & date create instead of plan db

--- razvozki/models.py
+++ razvozki/models.py
@@ -39,12 +39,15 @@ class Razvozka(models.Model):
     to_do_take = models.CharField(max_length=255, help_text='things to take from')
     map_point = models.CharField(max_length=255, help_text='Yandex mappoint')
     clr = models.CharField(max_length=30, default='', help_text='text color')
-    fulfilled = models.BooleanField(default=False, help_text='True is fulfilled')
+    fulfilled = models.BooleanField(default=True, help_text='True is fulfilled')
     deliver_to = models.BooleanField(default=False, help_text='transportation to processing')
     return_from = models.BooleanField(default=False, help_text='return products from processing')
     return_all = models.BooleanField(default=False, help_text='False if some part was not return')
     return_goods = models.ForeignKey('self', models.SET_NULL, null=True, blank=True,
                                      help_text='from which delivery return')
+    date_until = models.DateField(default='2022-08-01', null=True, blank=True, help_text='plan (last) date of transportation')
+    date_create = models.DateField(default='2022-08-01', null=True, blank=True, help_text='date of create razvozka')
+
 
 
     def __str__(self):
@@ -85,31 +88,3 @@ class Razvozka_import (models.Model):
                f"take={self.to_do_take!r}) "
 
 
-class Razvozka_plan (models.Model):
-    status = models.BooleanField(default=True, help_text='True if active False if moved to Razvozki')
-    date_create = models.DateField(null=True, blank=True, help_text='date when created')
-    date_until = models.DateField(null=True, blank=True, help_text='latest date for transportation')
-    customer = models.ForeignKey(Customer_clr, models.SET_NULL, null=True, blank=True)
-    customer_name = models.CharField(max_length=100)
-    address = models.CharField(max_length=255)
-    contact = models.CharField(max_length=255)
-    to_do_deliver = models.CharField(max_length=255, default='')
-    to_do_take = models.CharField(max_length=255, default='')
-    map_point = models.CharField(max_length=255)
-    deliver_to = models.BooleanField(default=False, help_text='transportation to processing')
-    return_from = models.BooleanField(default=False, help_text='return products from processing')
-    return_goods = models.ForeignKey('self', models.SET_NULL, null=True, blank=True,
-                                     help_text='from which delivery return')
-
-    def __repr__(self):
-        return f"razvozka_list(customer={self.customer_name!r}, deliver={self.to_do_deliver!r}, take={self.to_do_take!r}) "
-
-
-    def get_all_todo(self):
-        to_do_take = ''
-        to_do_deliver = ''
-        if self.to_do_take != '':
-            to_do_take = ' ЗАБРАТЬ: ' + str(self.to_do_take)
-        if self.to_do_deliver != '':
-            to_do_deliver = ' СДАТЬ: ' + str(self.to_do_deliver)
-        return f"{to_do_take} {to_do_deliver}"
