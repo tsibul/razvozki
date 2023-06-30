315beb32e3e504be052c059a26ee67a65f6eda4c
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-06-30 11:22:30 +0300

    preparing to publishgit pull

--- razv/settings_prd.py
+++ razv/settings_prd.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['yourdomain.com']  # Replace with your actual domain name
+ALLOWED_HOSTS = ['127.0.0.1', '10.0.1.16', 'localhost']  # Replace with your actual domain name
 
 # Application definition
 
