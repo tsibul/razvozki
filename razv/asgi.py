754ca05ba314625d1414ed03f15edc3e424929f1
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-06-30 07:03:20 +0300

    preparing to publish

--- razv/asgi.py
+++ razv/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'razv.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'razv.settings_prd')
 
 application = get_asgi_application()
--- razv/wsgi.py
+++ razv/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'razv.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'razv.settings_prd')
 
 application = get_wsgi_application()
